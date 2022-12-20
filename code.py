35e0e0e5ca2a06989fafb9a2fdc59d8c73f87f8f
Isaiah Fernandez <isaiah.fernandez@stu.ocsb.ca>
2022-12-20 18:47:25 +0000

    Added

---
+++ code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created by: Isaiah Fernandez
+# Date: October 11, 2022
+# This program is the space aliens program on PyBadge.
+
+#This function is the main game game_scene.
+def game_scene():
+   
+    #prints three blank lines.
+    print("\n\n\n")
+    print("Hello, World!")
+    
+    #Repeat forever
+    while True:
+        pass
+
+if __name__ == "__main__":
+    game_scene()
