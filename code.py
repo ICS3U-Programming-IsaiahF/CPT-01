eeb82ec3b66fe5ef78b9518de3eb9737a1763089
Isaiah Fernandez <isaiah.fernandez@stu.ocsb.ca>
2022-12-21 18:35:01 +0000

    Updated

--- code.py
+++ code.py
@@ -4,14 +4,32 @@
 # Date: December 19, 2022
 # This program is the space aliens program on PyBadge.
 
+import ugame
+import stage
+
+
 #This function is the main game game_scene.
 def game_scene():
-   
-    #prints three blank lines.
-    print("\n\n\n")
-    print("\033[31mHello, World!\033[0m")
     
-    #Repeat forever
+    #Image bank for circuit python
+    image_background_bank = stage.Bank.from_bmp16("space_aliens_background.bmp")
+
+    #Set background image to zero in the image bank
+    #And the size (10x8 of 16x16)
+    background = stage.Grid(image_background_bank, 10, 8)
+
+    #Create a stage for the background to show up on
+    #And set the frame rate to 60fps
+    game = stage.Stage(ugame.display, 60)
+
+    #Set the layers of all sprites
+    game.layers = [background]
+
+    #Render all sprites
+    #render the background once
+    game.render_block()
+    
+    #Repeat forever, game loop
     while True:
         pass
 
